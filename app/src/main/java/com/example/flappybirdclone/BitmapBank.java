774c94f3987a633e4ffcc09900f8f0e4d67c8903
Axel- <axelbastida.ab@gmail.com>
2022-05-25 11:27:18 -0700

    Initial commit; Added main Screen to the Flappy Bird Clone

---
+++ app/src/main/java/com/example/flappybirdclone/BitmapBank.java
@@ -0,0 +1,32 @@
+package com.example.flappybirdclone;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+public class BitmapBank {
+
+    Bitmap background_game;
+
+    public BitmapBank(Resources resources){
+
+        background_game = BitmapFactory.decodeResource(resources, R.drawable.background_game);
+
+    }
+
+    //return background bitmap
+    public Bitmap getBackground_game(){
+        return background_game;
+    }
+
+    //return background width
+    public int getBackgroundWidth(){
+        return background_game.getWidth();
+    }
+
+    //return background height
+    public int getBackgroundHeight(){
+        return background_game.getHeight();
+    }
+
+}
---
+++ app/src/main/java/com/example/flappybirdclone/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.flappybirdclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView playBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playBtn = findViewById(R.id.playBtn);
+
+        playBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Play!", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+}
